73daf31eb579fd54ae3c9c2dfc1940a90fe0dd34
Will Yeager <yeager.will@gmail.com>
2021-12-05 17:28:44 -0500

    Beginning Day5

---
+++ AOC21.UnitTests/Day5Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AOC21.UnitTests.Inputs
+{
+    [TestClass]
+    public class Day5Tests
+    {
+        [TestMethod]
+        public void Day5_Part1_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day5/Input.txt");
+
+            int actual = Day5.SolvePart1(input);
+            int expected = 0;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ AOC21/Day5.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AOC21
+{
+    public class Day5
+    {
+        public static int SolvePart1(string input)
+        {
+            return 0;
+        }
+    }
+}
