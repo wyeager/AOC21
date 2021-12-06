42e30091744fc004595f301928c36953043a5dc8
William Yeager <wyeager@leaporbit.com>
2021-12-06 15:00:00 -0500

    Solve Day6

--- AOC21.UnitTests/Day6Tests.cs
+++ AOC21.UnitTests/Day6Tests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,8 +16,30 @@ namespace AOC21.UnitTests
         {
             string input = Util.GetInput("Day6/Input.txt");
 
-            int actual = Day6.SolvePart1(input);
-            int expected = 0;
+            BigInteger actual = Day6.SolvePart1(input);
+            BigInteger expected = 383160;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Day6_Part2_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day6/Input.txt");
+
+            BigInteger actual = Day6.SolvePart2(input);
+            BigInteger expected = 1721148811504;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Day6_Part1Example_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day6/Example.txt");
+
+            BigInteger actual = Day6.SolvePart1(input);
+            BigInteger expected = 5934;
 
             Assert.AreEqual(actual, expected);
         }
--- AOC21/Day6.cs
+++ AOC21/Day6.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,50 @@ namespace AOC21
 {
     public class Day6
     {
-        public static int SolvePart1(string input)
+        public static BigInteger SolvePart1(string input)
         {
-            return 0;
+            return Solve(input, 80);
+        }
+
+        public static BigInteger SolvePart2(string input)
+        {
+            return Solve(input, 256);
+        }
+
+        public static BigInteger Solve(string input, int days)
+        {
+            var fishes = input
+                .Split(",")
+                .Select(s => int.Parse(s))
+                .GroupBy(s => s)
+                .ToDictionary(
+                    group => group.Key,
+                    group => new BigInteger(group.Count()));
+
+            for (int i = 0; i <= 8; i++)
+            {
+                // fill all fish days with 0 if they were not present in the puzzle input
+                fishes.TryAdd(i, 0);
+            }
+
+            for (int day = 1; day <= days; day++)
+            {
+                var overwritten = fishes[8];
+                for (int fishDay = 7; fishDay >= 0; fishDay--)
+                {
+                    var tempOverwritten = fishes[fishDay];
+                    fishes[fishDay] = overwritten;
+                    overwritten = tempOverwritten;
+
+                    if (fishDay == 0)
+                    {
+                        fishes[8] = overwritten;
+                        fishes[6] += overwritten;
+                    }
+                }
+            }
+
+            return fishes.Aggregate(new BigInteger(0), (acc, curr) => acc += curr.Value);
         }
     }
 }
