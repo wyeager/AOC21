4554d11d65f6f87708c9382561b51615919bc098
William Yeager <wyeager@leaporbit.com>
2021-12-07 13:45:04 -0500

    Solve Day7

---
+++ AOC21.UnitTests/Day7Tests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC21.UnitTests
+{
+    [TestClass]
+    public class Day7Tests
+    {
+        [TestMethod]
+        public void Day7_Part1_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day7/Input.txt");
+
+            int actual = Day7.SolvePart1(input);
+            int expected = 341534;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Day7_Part2_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day7/Input.txt");
+
+            int actual = Day7.SolvePart2(input);
+            int expected = 93397632;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ AOC21/Day7.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC21
+{
+    public class Day7
+    {
+        public static int SolvePart1(string input)
+        {
+            return Solve(input, (i) => i);
+        }
+
+        public static int SolvePart2(string input)
+        {
+            return Solve(input, (i) => Enumerable.Range(0, i + 1).Sum());
+        }
+
+        public static int Solve(string input, Func<int, int> fuelCalc)
+        {
+            List<int> positions = input
+                .Split(",")
+                .Select(s => int.Parse(s))
+                .ToList();
+
+            int minPosition = 0;
+            int minFuel = int.MaxValue;
+
+            for (int i = 0; i <= positions.Max(); i++)
+            {
+                int totalFuel = 0;
+                for (int j = 0; j < positions.Count; j++)
+                {
+                    totalFuel += fuelCalc(Math.Abs(positions[j] - i));
+                }
+
+                if (totalFuel < minFuel)
+                {
+                    minFuel = totalFuel;
+                    minPosition = i;
+                }
+            }
+
+            return minFuel;
+        }
+    }
+}
