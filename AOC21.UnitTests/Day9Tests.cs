184f3d62228d8fc171ffb80e18ca9a5e7cf86266
William Yeager <wyeager@leaporbit.com>
2021-12-10 14:20:16 -0500

    Solve Day9

--- AOC21.UnitTests/Day9Tests.cs
+++ AOC21.UnitTests/Day9Tests.cs
@@ -9,9 +9,34 @@ namespace AOC21.UnitTests
         public void Day9_Part1_ReturnsCorrect()
         {
             string input = Util.GetInput("Day9/Input.txt");
+            var day9 = new Day9();
 
-            int actual = Day9.SolvePart1(input);
+            int actual = day9.SolvePart1(input);
             int expected = 448;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Day9_Part2_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day9/Input.txt");
+            var day9 = new Day9();
+
+            int actual = day9.SolvePart2(input);
+            int expected = 1417248;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Day9_Part2Example_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day9/Example.txt");
+            var day9 = new Day9();
+
+            int actual = day9.SolvePart2(input);
+            int expected = 1134;
 
             Assert.AreEqual(actual, expected);
         }
--- AOC21/Day9.cs
+++ AOC21/Day9.cs
@@ -1,17 +1,18 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace AOC21
 {
     public class Day9
     {
-        public static int SolvePart1(string input)
+        bool[,] visited;
+        int rowLength;
+        int colLength;
+        int[][] grid;
+
+        public int SolvePart1(string input)
         {
-            int[][] grid = input
-                .Split("\r\n")
-                .Select(a => a
-                    .Select(b => int.Parse(b.ToString()))
-                    .ToArray())
-                .ToArray();
+            grid = ParseGrid(input);
 
             int result = 0;
 
@@ -52,9 +53,79 @@ namespace AOC21
             return result;
         }
 
-        public static int SolvePart2(string input)
+        public int SolvePart2(string input)
         {
-            return 0;
+            grid = ParseGrid(input);
+            rowLength = grid.Length;
+            colLength = grid[0].Length;
+            visited = new bool[rowLength, colLength];
+
+            var basinSizes = new List<int>();
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    int size = DFSUtil(i, j);
+                    if (size > 0)
+                    {
+                        basinSizes.Add(size);
+                    }
+                }
+            }
+
+            int result = basinSizes
+                .OrderByDescending(s => s)
+                .Take(3)
+                .Aggregate((acc, curr) => acc * curr);
+
+            return result;
+        }
+
+        private static int[][] ParseGrid(string input) =>
+            input
+                .Split("\r\n")
+                .Select(a => a
+                    .Select(b => int.Parse(b.ToString()))
+                    .ToArray())
+                .ToArray();
+
+        private int DFSUtil(int row, int col)
+        {
+            if (grid[row][col] == 9 || visited[row, col])
+            {
+                return 0;
+            }
+
+            // Mark the current node as visited
+            visited[row, col] = true;
+
+            // Recur for all the vertices
+            // adjacent to this vertex
+            var vList = new List<(int, int)> {
+                (row + 1, col),
+                (row - 1, col),
+                (row, col + 1),
+                (row, col - 1)
+            };
+
+            // count self
+            int size = 1;
+            foreach (var (r, c) in vList)
+            {
+                if (r < 0 || r >= rowLength || c < 0 || c >= colLength)
+                {
+                    continue;
+                }
+
+                if (!visited[r, c])
+                {
+                    // count all vertices this cell can reach (that haven't been visited or are 9)
+                    size += DFSUtil(r, c);
+                }
+            }
+
+            return size;
         }
     }
 }
