4f4a738ff9ab4e2c9a2247934a6e62c6635d112a
William Yeager <wyeager@leaporbit.com>
2021-12-10 15:16:14 -0500

    Solve Day10

---
+++ AOC21.UnitTests/Day10Tests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC21.UnitTests
+{
+    [TestClass]
+    public class Day10Tests
+    {
+        [TestMethod]
+        public void Day10_Part1_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day10/Input.txt");
+
+            int actual = Day10.SolvePart1(input);
+            int expected = 278475;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Day10_Part1Example_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day10/Example.txt");
+
+            int actual = Day10.SolvePart1(input);
+            int expected = 26397;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Day10_Part2_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day10/Input.txt");
+
+            long actual = Day10.SolvePart2(input);
+            long expected = 3015539998;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Day10_Part2Example_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day10/Example.txt");
+
+            long actual = Day10.SolvePart2(input);
+            long expected = 288957;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ AOC21/Day10.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC21
+{
+    public class Day10
+    {
+        public static int SolvePart1(string input)
+        {
+            List<string> lines = input
+                .Split("\r\n")
+                .ToList();
+
+            var parens = new Dictionary<char, char>
+            {
+                { '(', ')' },
+                { '[', ']' },
+                { '{', '}' },
+                { '<', '>' },
+            };
+
+            var points = new Dictionary<char, int>
+            {
+                { ')', 3 },
+                { ']', 57 },
+                { '}', 1197 },
+                { '>', 25137 },
+            };
+
+            int result = 0;
+            foreach (string line in lines)
+            {
+                var openers = new Stack<char>();
+
+                foreach (char c in line)
+                {
+                    if (parens.ContainsKey(c))
+                    {
+                        openers.Push(c);
+                    }
+                    else
+                    {
+                        char lastOpener = openers.Pop();
+
+                        if (parens[lastOpener] != c)
+                        {
+                            result += points[c];
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static long SolvePart2(string input)
+        {
+            List<string> lines = input
+                .Split("\r\n")
+                .ToList();
+
+            var parens = new Dictionary<char, char>
+            {
+                { '(', ')' },
+                { '[', ']' },
+                { '{', '}' },
+                { '<', '>' },
+            };
+
+            var points = new Dictionary<char, int>
+            {
+                { ')', 1 },
+                { ']', 2 },
+                { '}', 3 },
+                { '>', 4 },
+            };
+
+            var scores = new List<long>();
+            foreach (string line in lines)
+            {
+                var openers = new Stack<char>();
+                bool corrupted = false;
+
+                foreach (char c in line)
+                {
+                    if (parens.ContainsKey(c))
+                    {
+                        openers.Push(c);
+                    }
+                    else
+                    {
+                        char lastOpener = openers.Pop();
+
+                        if (parens[lastOpener] != c)
+                        {
+                            corrupted = true;
+                            break;
+                        }
+                    }
+                }
+
+                // skip corrupted lines
+                if (!corrupted)
+                {
+                    long score = 0;
+                    foreach (char c in openers)
+                    {
+                        score = (score * 5) + points[parens[c]];
+                    }
+
+                    scores.Add(score);
+                }
+            }
+
+            scores.Sort();
+            return scores[scores.Count / 2];
+        }
+    }
+}
