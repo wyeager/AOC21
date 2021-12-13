d6fef9131936587872725857c007dff5ffca8cc9
William Yeager <wyeager@leaporbit.com>
2021-12-13 11:35:06 -0500

    Solve Day12

---
+++ AOC21.UnitTests/Day12Tests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AOC21.UnitTests
+{
+    [TestClass]
+    public class Day12Tests
+    {
+        [TestMethod]
+        public void Day12_Part1_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day12/Input.txt");
+            var day12 = new Day12();
+
+            int actual = day12.SolvePart1(input);
+            int expected = 5104;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Day12_Part1Example1_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day12/Example1.txt");
+            var day12 = new Day12();
+
+            int actual = day12.SolvePart1(input);
+            int expected = 10;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Day12_Part2Example1_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day12/Example1.txt");
+            var day12 = new Day12();
+
+            int actual = day12.SolvePart2(input);
+            int expected = 36;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Day12_Part2_ReturnsCorrect()
+        {
+            string input = Util.GetInput("Day12/Input.txt");
+            var day12 = new Day12();
+
+            int actual = day12.SolvePart2(input);
+            int expected = 149220;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ AOC21/Day12.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AOC21
+{
+    public class Day12
+    {
+        readonly Dictionary<string, List<string>> AdjacencyList = new Dictionary<string, List<string>>();
+
+        public int SolvePart1(string input)
+        {
+            ParseInput(input);
+
+            var paths = DFS(
+                "start",
+                new List<string> { "start" },
+                (cave, path) => !path.Contains(cave) || IsUpper(cave));
+
+            return paths.Count;
+        }
+
+        public int SolvePart2(string input)
+        {
+            ParseInput(input);
+
+            var paths = DFS(
+                "start",
+                new List<string> { "start" },
+                (cave, path) => !path.Contains(cave) || IsUpper(cave) || !AlreadyContainsTwoSmallCaves(path));
+
+            return paths.Count;
+        }
+
+        private List<List<string>> DFS(string start, List<string> path, Func<string, List<string>, bool> canAdd)
+        {
+            // can never visit end twice
+            if (start == "end") return new List<List<string>>() { new List<string>(path) };
+
+            var result = new List<List<string>>();
+
+            foreach (var cave in AdjacencyList[start])
+            {
+                // can never visit start twice
+                if (cave == "start")
+                {
+                    continue;
+                }
+
+                if (canAdd(cave, path))
+                {
+                    result.AddRange(DFS(cave, new List<string>(path) { cave }, canAdd));
+                }
+            }
+
+            return result;
+        }
+
+        private static bool IsUpper(string str) =>
+            str.All(c => char.IsUpper(c));
+
+        private static bool AlreadyContainsTwoSmallCaves(List<string> path) =>
+            path
+                .Where(cave => cave != "start" && cave != "end" && !IsUpper(cave))
+                .GroupBy(cave => cave)
+                .ToDictionary(
+                    grouping => grouping.Key,
+                    grouping => grouping.ToList())
+                .Values
+                .Any(val => val.Count > 1);
+
+        private void ParseInput(string input)
+        {
+            var lines = input
+                .Split("\r\n");
+
+            foreach (var line in lines)
+            {
+                var start = line.Split("-")[0];
+                var end = line.Split("-")[1];
+
+                // add path from start to end vertex
+                if (!AdjacencyList.TryAdd(start, new List<string> { end }))
+                {
+                    AdjacencyList[start].Add(end);
+                }
+
+                // also add path from end to start vertex since the graph is not directed
+                if (!AdjacencyList.TryAdd(end, new List<string> { start }))
+                {
+                    AdjacencyList[end].Add(start);
+                }
+            }
+        }
+    }
+}
