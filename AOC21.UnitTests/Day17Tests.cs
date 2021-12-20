cc0f3bfbd05431c2e908c59208bb220d53fd51e0
Will Yeager <yeager.will@gmail.com>
2021-12-20 09:16:35 -0500

    Solve Day17

---
+++ AOC21.UnitTests/Day17Tests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AOC21.UnitTests
+{
+    [TestClass]
+    public class Day17Tests
+    {
+        [TestMethod]
+        public void Day17_Part1()
+        {
+            string input = Util.GetInput("Day17/Input.txt");
+
+            var (actual, _) = Day17.SolvePart1(input);
+            long expected = 10296;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Day17_Part1Example()
+        {
+            string input = Util.GetInput("Day17/Example.txt");
+
+            var (actual, _) = Day17.SolvePart1(input);
+            long expected = 45;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Day17_Part2()
+        {
+            string input = Util.GetInput("Day17/Input.txt");
+
+            var (_, actual) = Day17.SolvePart1(input);
+            long expected = 2371;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Day17_Part2Example()
+        {
+            string input = Util.GetInput("Day17/Example.txt");
+
+            var (_, actual) = Day17.SolvePart1(input);
+            long expected = 112;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ AOC21/Day17.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AOC21
+{
+    public class Day17
+    {
+        public static (long, long) SolvePart1(string input)
+        {
+            return Solve(input);
+        }
+
+        public static (long, long) SolvePart2(string input)
+        {
+            return Solve(input);
+        }
+
+
+        public static (long, long) Solve(string input)
+        {
+            var targetArea = ParseInput(input);
+
+            int overallMaxY = 0;
+            var initialVelocities = new HashSet<(int, int)>();
+
+            // I'm just choosing -150 and 1000 heuristically based on the puzzle input
+            for (int xVelocity = 0; xVelocity < 1000; xVelocity++)
+            {
+                for (int yVelocity = -150; yVelocity < 1000; yVelocity++)
+                {
+                    var (hit, localMaxY) = ReachesTargetArea(targetArea, xVelocity, yVelocity);
+
+                    if (hit)
+                    {
+                        initialVelocities.Add((xVelocity, yVelocity));
+                        overallMaxY = Math.Max(localMaxY, overallMaxY);
+                    }
+                }
+            }
+
+            return (overallMaxY, initialVelocities.Count);
+        }
+
+        private static (bool, int) ReachesTargetArea((int, int, int, int) targetArea, int xVelocity, int yVelocity)
+        {
+            var (xMin, xMax, yMin, yMax) = targetArea;
+
+            int x = 0;
+            int y = 0;
+            int maxY = 0;
+
+            while (x <= xMax && y >= yMin)
+            {
+                x += xVelocity;
+                y += yVelocity;
+
+                maxY = Math.Max(y, maxY);
+
+                xVelocity--;
+                // drag only brings the xVelocity to 0, it should never become negative
+                if (xVelocity < 0) xVelocity = 0;
+                yVelocity--;
+
+                // to be considered a hit the probe coordinates must be inside the target area after a descrete step
+                if (x >= xMin && x <= xMax && y >= yMin && y <= yMax)
+                {
+                    return (true, maxY);
+                }
+            }
+
+            return (false, maxY);
+        }
+
+        private static (int, int, int, int) ParseInput(string input)
+        {
+            var regex = new Regex(@"target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)", RegexOptions.Compiled);
+            var match = regex.Match(input);
+
+            int xMin = int.Parse(match.Groups[1].Value);
+            int xMax = int.Parse(match.Groups[2].Value);
+            int yMin = int.Parse(match.Groups[3].Value);
+            int yMax = int.Parse(match.Groups[4].Value);
+
+            return (xMin, xMax, yMin, yMax);
+        }
+    }
+}
